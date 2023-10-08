5dc16cbae6ccf0905f28821c385341466ee80baf
nicmackenzie <nicmackenzie@live.com>
2023-10-08 15:22:57 +0300

    App Theme setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.jsx';
 import './index.css';
+import { ThemeProvider } from './context/theme-provider.jsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider defaultTheme="light" storageKey="raia-ui-theme">
+        <App />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
