f04166ec2c02d83b39b5b9b0d073e9cf6f78267d
nicmackenzie <nicmackenzie@live.com>
2023-10-08 14:14:52 +0300

    add function to initialize a name

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(fullName) {
+  const words = fullName.split(/\s+/);
+  const firstNameInitial = words[0] ? words[0].charAt(0) : '';
+
+  const lastNameInitial =
+    words.length > 1 ? words[words.length - 1].charAt(0) : '';
+
+  return firstNameInitial + lastNameInitial;
+}
