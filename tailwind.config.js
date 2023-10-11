503027829b226052c1a902c855c97d3b06f5ea9c
nicmackenzie <nicmackenzie@live.com>
2023-10-11 08:33:49 +0300

    add gold color variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        gold: 'hsl(var(--gold))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
