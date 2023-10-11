942d664e95a144e7229adfcca34f7bd76538e482
nicmackenzie <nicmackenzie@live.com>
2023-10-11 14:57:55 +0300

    add service role

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,5 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
+const supabaseRoleKey = import.meta.env.VITE_SUPABASE_ANON;
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseRoleKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
