c8d4579c6bfc1b0c030ccb4f79c705076ce1bb79
nicmackenzie <nicmackenzie@live.com>
2023-10-12 14:10:25 +0300

    export supabaseUrl

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
 // const supabaseRoleKey = import.meta.env.VITE_SUPABASE_ANON;
 export const supabase = createClient(supabaseUrl, supabaseKey);
