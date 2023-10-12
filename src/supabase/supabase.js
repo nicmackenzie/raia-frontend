b67e59605772840151e6d21bf079d3f7f761a042
nicmackenzie <nicmackenzie@live.com>
2023-10-12 11:28:51 +0300

    set supabase to initial state

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
-const supabaseRoleKey = import.meta.env.VITE_SUPABASE_ANON;
-// export const supabase = createClient(supabaseUrl, supabaseKey);
-export const supabase = createClient(supabaseUrl, supabaseRoleKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
-});
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
+// const supabaseRoleKey = import.meta.env.VITE_SUPABASE_ANON;
+export const supabase = createClient(supabaseUrl, supabaseKey);
+// export const supabase = createClient(supabaseUrl, supabaseRoleKey, {
+//   auth: {
+//     autoRefreshToken: true,
+//     persistSession: true,
+//   },
+// });
