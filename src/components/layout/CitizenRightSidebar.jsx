7f5256e19cb3fabf1a75e87ba2269d37c21f9f4e
nicmackenzie <nicmackenzie@live.com>
2023-10-13 14:25:57 +0300

    change components rendering on sidebar

--- src/components/layout/CitizenRightSidebar.jsx
+++ src/components/layout/CitizenRightSidebar.jsx
@@ -1,27 +1,21 @@
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/Card';
+import InfluentialVoice from './InfluentialVoice';
+import { INFLUENTIAL_VOICES, LEADERS } from './constants';
+
 function CitizenRightSidebar() {
   return (
-    <div className="hidden lg:inline-flex lg:flex-col gap-y-4 w-1/4 ">
-      <Card className="w-full h-max">
-        <CardHeader className="p-2">
-          <CardTitle className="text-base">Upcoming Events</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-center text-xs text-muted-foreground">
-            No upcoming events
-          </p>
-        </CardContent>
-      </Card>
-      <Card className="w-full h-max">
-        <CardHeader className="p-2">
-          <CardTitle className="text-base">Popular discussions</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-center text-xs text-muted-foreground">
-            No discussions
-          </p>
-        </CardContent>
-      </Card>
+    <div className="hidden lg:inline-flex lg:flex-col gap-y-4 w-1/5">
+      <div className="w-full p-2 space-y-3">
+        <h3 className="text-sm font-bold text-tertiary">Top Voices</h3>
+        {INFLUENTIAL_VOICES.map(voice => (
+          <InfluentialVoice key={voice.username} {...voice} />
+        ))}
+      </div>
+      <div className="w-full p-2 space-y-3">
+        <h3 className="text-sm font-bold text-tertiary">Your Leaders</h3>
+        {LEADERS.map(voice => (
+          <InfluentialVoice key={voice.username} {...voice} leader />
+        ))}
+      </div>
     </div>
   );
 }
