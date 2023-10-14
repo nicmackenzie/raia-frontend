23be5b8b12a7ab7b8f66f1ce425f440b3867342b
Ivar-7 <leank6071@gmail.com>
2023-10-14 21:23:43 +0300

    added styling

--- src/features/news-updates/NewsUpdate.jsx
+++ src/features/news-updates/NewsUpdate.jsx
@@ -13,7 +13,7 @@ function NewsUpdate() {
       .then((response) => response.json())
       .then((data) => {
         setNewsUpdates(data);
-        console.log(data)
+        console.log(data);
       })
       .catch((error) => {
         console.error('Error fetching news and updates:', error);
@@ -32,26 +32,18 @@ function NewsUpdate() {
   }
 
   function handleEdit(id) {
-    fetch(`http://localhost:3000/news_and_updates/${id}/edit`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Editing news update:', data);
-        // You can navigate to an edit form or modal with the news update data
-      })
-      .catch((error) => {
-        console.error('Error fetching news update for editing:', error);
-      });
+    fetch(`http://localhost:3000/news_and_updates/${id}`);
+    // Add edit logic here
   }
 
   function handleDelete(id) {
-    // Implement the delete functionality here
     if (window.confirm('Are you sure you want to delete this news update?')) {
       fetch(`http://localhost:3000/news_and_updates/${id}`, {
         method: 'DELETE',
       })
         .then(() => {
           console.log(`News update with ID ${id} has been deleted.`);
-          // You may want to update the state or refresh the news update list
+          // Refresh the news update list
         })
         .catch((error) => {
           console.error('Error deleting news update:', error);
@@ -69,62 +61,89 @@ function NewsUpdate() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let newObject = {...formData, county_id: 1, user_id: 1, published_date: '2023-10-14'}
+    let newObject = {
+      ...formData,
+      county_id: 1,
+      user_id: 1,
+      published_date: '2023-10-14',
+    };
     fetch('http://localhost:3000/news_and_updates', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newObject),
     })
-    .then((response) => response.json() )
-    .then((data) => {
-        // console.log(data)
-        let updatedNews = [...newsUpdates, data]
-        setNewsUpdates(updatedNews)
-    })
-    .catch((error) => {
+      .then((response) => response.json())
+      .then((data) => {
+        let updatedNews = [...newsUpdates, data];
+        setNewsUpdates(updatedNews);
+      })
+      .catch((error) => {
         console.error('Error Posting news update:', error);
-    });
+      });
   };
 
   return (
-    <div>
-      <h1>News and Updates</h1>
-      <ul>
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4">News and Updates</h1>
+      <ul className="space-y-4">
         {newsUpdates.map((newsUpdate) => (
-          <li key={newsUpdate.id}>
-            {newsUpdate.title}
-            <button onClick={() => handleShow(newsUpdate.id)}>Show</button>
-            <button onClick={() => handleEdit(newsUpdate.id)}>Edit</button>
-            <button onClick={() => handleDelete(newsUpdate.id)}>Delete</button>
+          <li key={newsUpdate.id} className="bg-white rounded-lg shadow-md p-4">
+            <h2 className="text-xl font-bold mb-2">{newsUpdate.title}</h2>
+            <p className="text-gray-500 text-sm mb-2">
+              Published Date: {newsUpdate.published_date}
+            </p>
+            <p className="text-gray-500 text-sm mb-2">Content: {newsUpdate.content}</p>
+            <p className="text-gray-500 text-sm mb-2">County ID: {newsUpdate.county_id}</p>
+            <p className="text-gray-500 text-sm mb-2">User ID: {newsUpdate.user_id}</p>
+            <div className="flex space-x-2">
+              <button
+                onClick={() => handleShow(newsUpdate.id)}
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+              >
+                Show
+              </button>
+              <button
+                onClick={() => handleEdit(newsUpdate.id)}
+                className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDelete(newsUpdate.id)}
+                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
       <div className="mt-8">
-        <h2 className="text-xl font-semibold mb-4">Create or Update News Update</h2>
+        <h2 className="text-2xl font-semibold mb-4">Create News</h2>
         <form onSubmit={handleSubmit} className="max-w-md">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+            <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
               Title:
             </label>
             <input
-              className="border rounded w-full py-2 px-3"
               type="text"
               name="title"
               value={formData.title}
               onChange={handleChange}
               id="title"
+              className="border rounded w-full py-2 px-3"
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">
+            <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">
               Content:
             </label>
             <textarea
-              className="border rounded w-full py-2 px-3 h-40"
               name="content"
               value={formData.content}
               onChange={handleChange}
               id="content"
+              className="border rounded w-full py-2 px-3 h-40"
             />
           </div>
           <button
