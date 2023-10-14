53c18d8a7b7534b44e833eaef9ca5c0d78f29221
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-14 13:45:51 +0300

    adding event form

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -8,18 +8,21 @@ function Events() {
   const [tgl,setTgl] = useState(new Date())
   const [showEventDetails,setShowEventDetails]=useState(false)
   const [event,setEvent]=useState(null)
+  const [createEvent,setCreateEvent] = useState(false)
 const events = [
   { date: '18-10-2023', event: 'Event A' },
   { date: '19-10-2023', event: 'Event B' },
   { date: '16-10-2023', event: 'Event C' },
   { date: '17-10-2023', event: 'Event D' },
   { date: '10-10-2023', event: 'Event E' },
-  // Add more events as needed
 ];
   // console.log(tgl)
   function handleEventClick(){
     console.log(event)
   }
+function handleCreateEvent(){
+  setCreateEvent(true)
+}
   function handleDateClick(date){
     console.log(date)
     const isHighlighted =  events.some((day) => {
@@ -42,7 +45,7 @@ const events = [
   }
   return <div>
     <div>
-      <Button children={"Create Event"}/>
+      <Button children={"Create Event"} onClick={handleCreateEvent}/>
     </div>
     <Calendar
   // className='w-96 h-64 rounded-xl mb-6 bg-blue-300 react-calendar'
@@ -67,6 +70,19 @@ const events = [
     <h3>Upcoming event</h3>
     {event}
   </div>
+}
+{
+  createEvent && <form className="event-form">
+    <label htmlFor="name">Enter event name:</label>
+    <input name="name" type="text"></input>
+    <label htmlFor="description">Enter event description:</label>
+    <input name="description" type="text"></input>
+    <label htmlFor="date">Enter date for event</label>
+    <input name="date" type="date"></input>
+    <label htmlFor="county">Enter specific county</label>
+    <input name="county" type="text"></input>
+    <button type="submit">Submit</button>
+  </form>
 }
   </div>
 }
