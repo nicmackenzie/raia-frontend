f43e6f394bf6220a1d6d79fe1f2be4915bff4873
nicmackenzie <nicmackenzie@live.com>
2023-10-17 14:24:42 +0300

    custom render leaders

--- src/components/layout/CitizenRightSidebar.jsx
+++ src/components/layout/CitizenRightSidebar.jsx
@@ -1,9 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
+import { Loader } from 'lucide-react';
 import InfluentialVoice from './InfluentialVoice';
 import { getTopVoicesAndLeaders } from '../../services/profile-api';
-import { Loader } from 'lucide-react';
+import { useRole } from '../../hooks/use-role';
+import { useUser } from '../../features/authentication/use-user';
 
 function CitizenRightSidebar() {
+  const role = useRole();
+  const { data: user } = useUser();
+
   const { data, isLoading } = useQuery({
     queryFn: getTopVoicesAndLeaders,
     queryKey: ['not-following', 'leaders'],
@@ -32,26 +37,28 @@ function CitizenRightSidebar() {
           </p>
         )}
       </div>
-      <div className="w-full p-2 space-y-3">
-        <h3 className="text-sm font-bold text-tertiary">Your Leaders</h3>
-        {data?.leaders?.length > 0 ? (
-          data?.leaders?.map(voice => (
-            <InfluentialVoice
-              key={voice.id}
-              avatar={voice.profile_image}
-              averageRating={0}
-              position={voice.elected_position}
-              username={voice.username}
-              fullName={voice.full_name}
-              leader
-            />
-          ))
-        ) : (
-          <p className="text-center text-muted-foreground text-xs">
-            Unable to get your leaders.
-          </p>
-        )}
-      </div>
+      {role === 'citizen' && !!user.user.county_id && (
+        <div className="w-full p-2 space-y-3">
+          <h3 className="text-sm font-bold text-tertiary">Your Leaders</h3>
+          {data?.leaders?.length > 0 ? (
+            data?.leaders?.map(voice => (
+              <InfluentialVoice
+                key={voice.id}
+                avatar={voice.profile_image}
+                averageRating={0}
+                position={voice.elected_position}
+                username={voice.username}
+                fullName={voice.full_name}
+                leader
+              />
+            ))
+          ) : (
+            <p className="text-center text-muted-foreground text-xs">
+              Unable to get your leaders.
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
