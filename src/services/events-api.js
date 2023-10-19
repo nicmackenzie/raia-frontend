3a5b2c39b596d03afab59d100ab3cff34323cab5
nicmackenzie <nicmackenzie@live.com>
2023-10-19 23:33:55 +0300

    addd poster to event creation

--- src/services/events-api.js
+++ src/services/events-api.js
@@ -1,8 +1,23 @@
 import { httpRequest, url } from '../lib/utils';
+import { supabase, supabaseUrl } from '../supabase/supabase';
 
 export async function createEvent(details) {
+  const fileName = `${Math.random()}-${details.poster[0].name}`.replaceAll(
+    '/',
+    ''
+  );
+  const filePath = `${supabaseUrl}/storage/v1/object/public/events/${fileName}`;
+  const { error } = await supabase.storage
+    .from('events')
+    .upload(fileName, details.poster[0]);
+  if (error) throw new Error(error.message);
+  const formFields = {
+    ...details,
+    poster_url: filePath,
+  };
+  delete formFields.poster;
   try {
-    await httpRequest(url + '/events', 'POST', JSON.stringify(details));
+    await httpRequest(url + '/events', 'POST', JSON.stringify(formFields));
   } catch (error) {
     throw new Error(error.message);
   }
