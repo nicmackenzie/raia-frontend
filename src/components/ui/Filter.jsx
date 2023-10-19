a841a2f9ec1a36a345d90cf47facb7770b134707
nicmackenzie <nicmackenzie@live.com>
2023-10-19 15:29:41 +0300

    reusable component

---
+++ src/components/ui/Filter.jsx
@@ -0,0 +1,33 @@
+import { useSearchParams } from 'react-router-dom';
+import Button from './Button';
+import { cn } from '../../lib/utils';
+
+export default function Filter({ options, filterKey }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const selectedTab = searchParams.get(filterKey) || options.at(0)?.value;
+
+  function handleClick(value) {
+    searchParams.set(filterKey, value);
+    if (searchParams.get('page')) searchParams.set('page', '1');
+    setSearchParams(searchParams);
+  }
+  return (
+    <div className="p-1 border rounded flex gap-1 items-center">
+      {options.map(option => (
+        <Button
+          key={option.value}
+          className={cn(
+            'py-2',
+            selectedTab === option.value
+              ? 'bg-primary/10 text-primary hover:bg-primary/20'
+              : 'bg-gray-50 dark:bg-gray-800 dark:text-gray-50  text-gray-800 hover:bg-primary/10 hover:text-primary dark:hover:bg-gray-900'
+          )}
+          //   {selectedTab === option.value ? 'secondary' : 'ghost'}
+          onClick={() => handleClick(option.value)}
+        >
+          {option.label}
+        </Button>
+      ))}
+    </div>
+  );
+}
