977893d90c24c77ae52fc72adea8b35e684f2f1c
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-20 20:10:20 +0300

    rendering attending users in event details

--- src/pages/EventDetail.jsx
+++ src/pages/EventDetail.jsx
@@ -1,15 +1,39 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import Button from "../components/ui/Button";
+import { JapaneseYen } from "lucide-react";
 
 
 function EventDetail() {
   const [confirmed,setConfirmed]=useState(false)
   const { id } = useParams();
   const eventId = parseInt(id); // Convert id to a number
-
+  const [users,setUsers]=useState([])
+  const [seeAttendees,setSeeAttendees]=useState(false)
+const [inquiring,setInquiring]=useState(false)
   const [event, setEvent] = useState({});
-
+  const usersInfo = [{
+    id:1,
+    name: 'Jane Smith',
+    age: 25,
+    email: 'jane.smith@example.com',
+    address: '456 Oak Ave, Townsville, USA',
+  },
+{
+  id:2,
+  name: 'James Brown',
+    age: 40,
+    email: 'james.brown@example.com',
+    address: '789 Maple Ln, Villageton, USA',
+},{
+  id:3,
+  name: 'Jill Johnson',
+  age: 35,
+  email: 'jill.johnson@example.com',
+  address: '101 Pine Rd, Hilltop, USA',
+}]
+  
+  
   const events = [
     {
       "id": 1,
@@ -53,25 +77,70 @@ function EventDetail() {
 
   function handleConfirmation(){
     // handle confirmation logic to backend
-    setConfirmed(true)
+    const eventDetail={
+      user_id:1,
+      eventId:id,
+      is_attending:true
+    }
+    fetch(" http://localhost:3000/event_details",{
+      method:"POST",
+      headers:{
+        "Content-type":"application/json"
+      },
+      body:JSON.stringify(eventDetail)
+    }).then(res=>{
+      if(res.ok){
+        setConfirmed(true)
+      }
+      res.json()})
+    .then(data=>console.log(data))
+    
   }
   function fetchAllAttenders(){
     // fetch all people attending backend
-    console.log(event)
+    setSeeAttendees(true)
+    setUsers(usersInfo)
+    // fetch(`http://localhost:3000/event_details/${id}`)
+    // .then(res=>res.json())
+    // .then(data=>setUsers(data))
+  }
+  function handleInquiry(){
+setInquiring(()=>true)
+  }
+  function handleSubmitInquiry(e){
+    e.preventDefault()
   }
 
   return (
     <div>
       <div className="nav-buttons">
       <Button children={confirmed ?"Attendance confirmed":"Confirm attendance"} onClick={handleConfirmation}/>
-      <Button children={"See attendees"} onclick={fetchAllAttenders}/>
-      <Button children={"inquire about event"}/>
+      <Button children={"See attendees"} onClick={fetchAllAttenders}/>
+      <Button children={"inquire about event"} onClick={handleInquiry}/>
       </div>
     <div className="event-card">
       <h3>{event.name}</h3>
       <p>{event.description}</p>
       <p>{event.county}</p>
     </div>
+   
+    {inquiring && <form onSubmit={handleSubmitInquiry} className="event-form" >
+      <label htmlFor="inquiry">Ask about event here:</label>
+      <textarea name="inquiry" type="text"></textarea>
+    <Button children={"send inquiry"} type="submit"/>
+    </form>}
+    {seeAttendees && users.length>0 && users.map((user)=>{
+      return <div className="user-cards" key={user.id}>
+        <p>name: {user.name}</p>
+        <p>email: {user.email}</p>
+      </div>
+    })}
+    {
+      seeAttendees && users.length===0 && <div className="user-cards">
+        <p>No users have confirmed yet</p>
+      </div>
+    }
+    
     </div>
   );
 }
