880a76adb5e0b50a1ea025cb89166bcc0e86657c
nicmackenzie <nicmackenzie@live.com>
2023-11-14 22:06:52 +0300

    fetch ticket

--- src/services/tickets-api.js
+++ src/services/tickets-api.js
@@ -1,8 +1,8 @@
-import { httpRequest, url } from '../lib/utils';
+import { httpRequest, secUrl } from '../lib/utils';
 
 export async function createTicket(details) {
   try {
-    await httpRequest(url + '/tickets', 'POST', JSON.stringify(details));
+    await httpRequest(secUrl + '/tickets', 'POST', JSON.stringify(details));
   } catch (error) {
     throw new Error(error.message);
   }
@@ -11,8 +11,18 @@ export async function createTicket(details) {
 export async function getTicketsByUser(user) {
   if (!user) return [];
   try {
-    const tickets = await httpRequest(url + '/tickets/' + user + '/user');
+    const tickets = await httpRequest(secUrl + '/tickets');
     return tickets;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getTicket(ticketId) {
+  if (!ticketId) throw new Error('Ticket idd not defined');
+  try {
+    const ticket = await httpRequest(secUrl + '/tickets/' + ticketId);
+    return ticket;
   } catch (error) {
     throw new Error(error.message);
   }
